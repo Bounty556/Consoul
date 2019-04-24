4440393bf333558981021f89470b0de22a52f793
Bounty556 <jacobmayday@gmail.com>
2019-04-24 00:17:00 -0600

    Added Basic Windowing

---
+++ Consoul/src/Soul/Window.cpp
@@ -0,0 +1,66 @@
+#include "Window.h"
+
+#include <cstdio>
+
+namespace Soul {
+	Window::Window(int width, int height)
+		: m_BufferWidth(width), m_BufferHeight(height), m_BufferSize(width * height),
+		m_Screen(new wchar_t[m_BufferSize]),
+		m_Bytes(0),
+		m_Running(true)
+	{
+		m_Console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL,
+			CONSOLE_TEXTMODE_BUFFER, NULL);
+
+		// Error handling
+		if (m_Console == INVALID_HANDLE_VALUE)
+		{
+			m_Running = false;
+			swprintf_s(m_Screen, 160, L"ERROR [%d]: Could not create window", GetLastError());
+		}
+		else
+		{
+			SetConsoleActiveScreenBuffer(m_Console);
+		}
+	}
+
+	Window::~Window()
+	{
+		CloseHandle(m_Console);
+		delete[] m_Screen;
+	}
+
+	void Window::DrawToBuffer(wchar_t wchar, int x, int y)
+	{
+		if (x < 0 || x >= m_BufferWidth || y < 0 || y >= m_BufferHeight)
+		{
+			// Report error
+			return;
+		}
+
+		m_Screen[y * m_BufferWidth + x] = wchar;
+	}
+
+	void Window::DrawToBuffer(const wchar_t* wstring, int length, int x, int y)
+	{
+		if (x < 0 || x >= m_BufferWidth || y < 0 || y >= m_BufferHeight ||
+			length + y * m_BufferWidth + x >= m_BufferSize)
+		{
+			// Report error
+			return;
+		}
+
+		for (int i = 0; i < length; ++i)
+			m_Screen[i + y * m_BufferWidth + x] = wstring[i];
+	}
+
+	void Window::DrawFrame()
+	{
+		m_Screen[m_BufferWidth * m_BufferHeight - 1] = '\0';
+		WriteConsoleOutputCharacterW(m_Console, m_Screen, m_BufferSize, { 0, 0 }, &m_Bytes);
+	}
+
+	void Window::Update(float deltaTime)
+	{
+	}
+}
---
+++ Consoul/src/Soul/Window.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <Windows.h>
+
+namespace Soul {
+	class Window
+	{
+	public:
+		Window(int width, int height);
+		~Window();
+
+		void DrawToBuffer(wchar_t glyph, int x, int y);
+		void DrawToBuffer(const wchar_t* glyph, int length, int x, int y);
+
+		void DrawFrame();
+		void Update(float deltaTime);
+
+		inline int GetWidth() const { return m_BufferWidth; }
+		inline int GetHeight() const { return m_BufferHeight; }
+		inline int GetSize() const { return m_BufferSize; }
+		inline int IsRunning() const { return m_Running; }
+	private:
+		int m_BufferWidth;
+		int m_BufferHeight;
+		int m_BufferSize;
+		wchar_t* m_Screen;
+		HANDLE m_Console;
+		DWORD m_Bytes;
+		bool m_Running;
+	};
+}
